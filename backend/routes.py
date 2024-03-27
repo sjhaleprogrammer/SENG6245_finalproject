24f192270bdba467e74ec0e2a002087c51553709
Samuel Hale <samworlds1231337@gmail.com>
2024-03-27 17:42:40 -0400

    added routes to backend project

---
+++ backend/routes.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render        
+from django.http import HttpResponse
+    
+
+# method to call weather api    
+def weatherapicall(request):    
+    return
+
+
+# method to call chatapi api
+def chatapicall(request):
+    return
+
+
