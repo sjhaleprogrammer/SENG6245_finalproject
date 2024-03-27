dbdac6e3daf1d044cf59b2d5eb3efb6a331020e6
Samuel Hale <samworlds1231337@gmail.com>
2024-03-27 18:17:34 -0400

    added home route

--- backend/routes.py
+++ backend/routes.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render        
 from django.http import HttpResponse
-    
+   
+def home(request):
+    return HttpResponse("Hello, welcome to the database ")
 
 # method to call weather api    
 def weatherapicall(request):    
