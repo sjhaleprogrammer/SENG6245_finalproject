49490c06f3fdb22e521dd99b9f5bbe20cb638f7c
Sachin Kumar <sachin.15@hotmail.com>
2024-04-28 23:51:19 -0400

    COmmenting nested class in order to run the application locally

--- django-database/weather/models.py
+++ django-database/weather/models.py
@@ -11,8 +11,10 @@ class Weather(models.Model):
     def create_weather(cls, weather_date, temperature_data):
         return cls(weather_date=weather_date, temperature_data=temperature_data)
 
+    """
     class Meta:
         app_label = 'weather.apps.WeatherConfig'
+    """
    
 
 class AISummary(models.Model):
@@ -23,7 +25,8 @@ class AISummary(models.Model):
     def __str__(self):
         return f"ChatGPT Summary for {self.weather.weather_date} ({self.created_at})"
     
+    """    
     class Meta:
         app_label = 'weather.apps.WeatherConfig'
-   
+   """
 
