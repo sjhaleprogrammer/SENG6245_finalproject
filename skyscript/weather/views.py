1d1bbf38fced36682ae5efdec3686ac2b567eeaa
Coolcody1029 <cody.criner35@yahoo.com>
2024-04-30 00:51:27 -0400

    Upgraded The UI, added Date, Current tempature, Min and Max tempature and some css

--- skyscript/weather/views.py
+++ skyscript/weather/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from datetime import date
 from .models import AISummary
+from .apps import get_weather_data
+from datetime import datetime
 
 def index(request):
     
     data = AISummary.objects.filter(created_at= date.today() )
+
+    temperature = get_weather_data()
+
+    current_date = datetime.now().date()
+
+    min_temperature = min(temperature)
+
+    max_temperature = max(temperature)
     
-    return render(request, 'index.html', {'processed_data': data[0].text})
+    return render(request, 'index.html', {
+        'processed_data': data[0].text,
+        'temperature': temperature[0],
+        'current_date': current_date,
+        'min_temperature': min_temperature,
+        'max_temperature': max_temperature
+    })
+    
+    
